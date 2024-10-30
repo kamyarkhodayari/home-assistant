dce67e92095d67a73f7b514e6197ebe7350d1c0c
Kamyar Khodayari <kamyar@royan.co>
2024-10-31 00:08:32 +0100

    wip: disable grid system for tailwind

--- src/features/common/components/Widget.tsx
+++ src/features/common/components/Widget.tsx
@@ -2,7 +2,7 @@ import { Widget } from "../types";
 
 const WidgetCard = ({ cols, children }: Widget) => {
   return (
-    <div className={`col-span-${cols} bg-white shadow-lg w-full`}>
+    <div className={`bg-white shadow-lg w-full`}>
       {children}
     </div>
   );
